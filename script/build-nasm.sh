7ed19952a21556c17f0d190afdd768641bdb04a0
miku552 <xuboyun20040417@126.com>
2025-04-17 11:50:17 +0800

    Fix nasm

--- script/build-nasm.sh
+++ script/build-nasm.sh
@@ -72,6 +72,7 @@ checkStatus $? "build failed"
 # --- FIX: Create dummy man page to prevent install error ---
 echo "Creating dummy nasm.1 to prevent install error"
 touch nasm.1
+touch ndisasm.1
 # --- End FIX ---
 
 # install
